0b6f6f53dcbfd03623c3f68c693e8f1789eba34f
Yogesh Gupta <yogeshgupta@Mac.lan>
2025-06-20 12:38:59 +0530

    360-images-added-home-page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Companies from '../components/Companies'
-import Features from '../components/Features'
-import Properties from '../components/propertiesshow'
-import Steps from '../components/Steps'
-import Testimonials from '../components/testimonial'
-import Blog from '../components/Blog'
-import PanoramaViewer from '../components/PanoramaViewer'
+import React from "react";
+import Hero from "../components/Hero";
+import Companies from "../components/Companies";
+import Features from "../components/Features";
+import Properties from "../components/propertiesshow";
+import Steps from "../components/Steps";
+import Testimonials from "../components/testimonial";
+import Blog from "../components/Blog";
+import PanoramaViewer from "../components/PanoramaViewer";
 
 const Home = () => {
   // Static 360° scenes for the home page
   const staticScenes = [
     {
-      id: 1,
-      image_url: "https://housemateone.s3.us-east-1.amazonaws.com/documents/1749994992516-WhatsApp_Image_2025-06-14_at_23.27.18.jpeg",
-      room_id: null,
-      created_at: null,
-      hotspots: []
+      id: 5,
+      image_url:
+        "https://housemateone.s3.us-east-1.amazonaws.com/documents/1750403025623-Property_360_Home1.jpeg",
+      room_id: 19,
+      created_at: "2025-06-20T07:03:48.989Z",
+      hotspots: [],
     },
     {
-      id: 2,
-      image_url: "https://housemateone.s3.us-east-1.amazonaws.com/documents/1749994992516-WhatsApp_Image_2025-06-14_at_23.27.18.jpeg",
-      room_id: null,
-      created_at: null,
-      hotspots: []
-    }
+      id: 6,
+      image_url:
+        "https://housemateone.s3.us-east-1.amazonaws.com/documents/1750403035792-Property_360_Home2.jpeg",
+      room_id: 19,
+      created_at: "2025-06-20T07:03:58.207Z",
+      hotspots: [],
+    },
+    {
+      id: 7,
+      image_url:
+        "https://housemateone.s3.us-east-1.amazonaws.com/documents/1750403078428-Property_360_Home3.jpeg",
+      room_id: 19,
+      created_at: "2025-06-20T07:04:41.858Z",
+      hotspots: [],
+    },
   ];
 
   return (
@@ -39,11 +49,12 @@ const Home = () => {
               Experience Our Properties in 360°
             </h2>
             <p className="text-lg text-gray-600 max-w-2xl mx-auto">
-              Take a virtual tour of our premium properties and explore every corner from the comfort of your home.
+              Take a virtual tour of our premium properties and explore every
+              corner from the comfort of your home.
             </p>
           </div>
-          <PanoramaViewer 
-            staticScenes={staticScenes} 
+          <PanoramaViewer
+            staticScenes={staticScenes}
             className="w-full h-[600px] rounded-xl shadow-lg"
           />
         </div>
@@ -53,7 +64,7 @@ const Home = () => {
       <Testimonials />
       <Blog />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
